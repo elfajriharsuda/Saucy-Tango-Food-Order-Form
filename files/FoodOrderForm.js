0bb6667f19fadb0444d29dbf66c10cad4a5f4dfc
Elfajri Harsuda <elfajri.harsuda@gmail.com>
2024-05-12 23:42:57 +0700

    Add files

---
+++ files/FoodOrderForm.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function FoodOrderForm() {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [order, setOrder] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <form>
+      <label htmlFor="name">Name :</label>
+      <input 
+        id="name" 
+        value={name} 
+        onChange={(e) => setName(e.target.value)} 
+      />
+      <label htmlFor="phone">Phone Number :</label>
+      <input
+        id="phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <label htmlFor="address">Address :</label>
+      <input
+        id="address"
+        value={address}
+        onChange={(e) => setAddress(e.target.value)}
+      />
+      <label htmlFor="order">Order :</label>
+      <input
+        id="order"
+        value={order}
+        onChange={(e) => setOrder(e.target.value)}
+      />
+      <button type="submit">Submit Order</button>
+    </form>
+  );
+}
+
+export default FoodOrderForm;
